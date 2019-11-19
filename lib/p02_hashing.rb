d35e408687390abd18cdfbaa5a5de53bfaffd770
J Resilient <j.resilient.1@gmail.com>
2019-11-19 14:07:39 -0500

    Take out calls to self

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -12,7 +12,7 @@ end
 
 class String
   def hash
-    self.split("").map(&:ord).hash
+    split("").map(&:ord).hash
   end
 end
 
@@ -20,6 +20,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    self.sort.hash
+    sort.hash
   end
 end
