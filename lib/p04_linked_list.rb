966beb7e7dfb696817bac753c43c6b3fee8fd7e3
J Resilient <j.resilient.1@gmail.com>
2019-11-20 13:51:45 -0500

    Finish code to pass tests

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -35,34 +35,52 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail && @tail.prev == @head
   end
 
   def get(key)
+    key_node = self.find { |node| node.key == key }
+    key_node.val unless key_node.nil?
   end
 
   def include?(key)
+    self.each { |node| return true if node.key == key }
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    new_node.prev, new_node.next = @tail.prev, @tail
+    @tail.prev.next, @tail.prev = new_node, new_node
   end
 
   def update(key, val)
+    self.each { |node| node.val = val if node.key == key }
   end
 
   def remove(key)
+    node = self.find { |n| n.key == key }
+    node.remove
   end
 
   def each
+    node = @head.next
+    until node == @tail
+      yield ( node )
+      node = node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
