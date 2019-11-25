30e41c53f3c56e6ccbe8fa3984402c6a2f8c2f71
J Resilient <j.resilient.1@gmail.com>
2019-11-25 12:13:42 -0500

    Add code to pass specs

--- lib/p07_dynamic_array_bonus.rb
+++ lib/p07_dynamic_array_bonus.rb
@@ -27,54 +27,113 @@ class StaticArray
 end
 
 class DynamicArray
+  include Enumerable
   attr_accessor :count
-
+  
   def initialize(capacity = 8)
     @store = StaticArray.new(capacity)
     @count = 0
   end
 
-  def [](i)
-  end
-
-  def []=(i, val)
-  end
-
   def capacity
     @store.length
   end
+  
+  def [](i)
+    i = count + i if i < 0
+    return @store[i] if i >= 0 && i < capacity
+    nil
+  end
+  
+  def []=(i, val)
+    # handle negative indices
+    i = count + i if i < 0
+    # increment count depending on index
+    # and whether we're replacing a value or adding a new one
+    new_count = count
+    if i > count
+      new_count = i + 1
+    elsif count == i
+      # we're adding a new element to the end
+      new_count = count + 1
+    end
+    # if you reset count before running #resize
+    # you get an out of bounds error
+    resize! until new_count <= capacity
+    @count = new_count
+    @store[i] = val
+  end
 
   def include?(val)
+    each { |el| return true if el == val }
+    false
   end
 
   def push(val)
+    self[count] = val
+    val
   end
 
   def unshift(val)
+    i = count
+    until i == 0
+      self[i] = self[i - 1]
+      i -= 1
+    end
+    self[0] = val
+    val
   end
 
   def pop
+    return nil if count == 0
+    el = last
+    # if you swap the order of these two lines, count is incremented 
+    # incorrectly in []=
+    self[count - 1] = nil
+    @count -= 1
+    el
   end
-
+  
   def shift
+    return nil if count == 0
+    el = first
+    (1...count).each do |i| 
+      self[i - 1] = self[i]
+    end
+    # if you swap the order of these two lines, count is incremented 
+    # incorrectly in []=
+    self[count - 1] = nil
+    @count -= 1
+    el
   end
 
   def first
+    self[0]
   end
 
   def last
-  end
-
-  def each
-  end
-
-  def to_s
-    "[" + inject([]) { |acc, el| acc << el }.join(", ") + "]"
+    self[-1]
   end
 
   def ==(other)
     return false unless [Array, DynamicArray].include?(other.class)
-    # ...
+    return false unless @count == other.length
+    each_with_index { |el, i| return false unless el == other[i] }
+    true
+  end
+
+  def each
+    i = 0
+    while i < count
+      el = @store[i]
+      yield( el )
+      i += 1
+    end
+    self
+  end
+
+  def to_s
+    "[" + inject([]) { |acc, el| acc << el }.join(", ") + "]"
   end
 
   alias_method :<<, :push
@@ -83,5 +142,13 @@ class DynamicArray
   private
 
   def resize!
+    old_array = @store
+    @store = StaticArray.new(capacity * 2)
+    num_els = count
+    @count = 0
+    until count == num_els
+      @store[count] = old_array[count]
+      @count += 1
+    end
   end
 end
